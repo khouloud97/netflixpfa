eb3a3e00dfe6379edaadf976b0575e1d210a9d93
khouloud97 <khouloudachour.97@gmail.com>
2021-04-20 01:19:51 +0100

    Update Forgotpassword.js

--- src/screens/Forgotpassword.js
+++ src/screens/Forgotpassword.js
@@ -1,5 +1,6 @@
 import React, { useRef } from "react";
 import { useHistory } from "react-router";
+import { Link } from "react-router-dom";
 import { auth } from "../firebase";
 import "./Forgotpassword.css";
 
@@ -18,11 +19,14 @@ function Forgotpassword() {
 
   return (
     <>
-      <img
-        className="nav_logo"
-        src="https://scontent.xx.fbcdn.net/v/t1.15752-0/p403x403/157181232_260601775599893_4404483419325055995_n.png?_nc_cat=102&ccb=1-3&_nc_sid=f79d6e&_nc_ohc=XrMKlKV7EwEAX-LV-Ms&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&tp=30&oh=72feb42faa0fdd193470e1be64cb72b5&oe=60A4DB67"
-        alt=""
-      />
+      <Link to="/">
+        {" "}
+        <img
+          className="nav_logo"
+          src="https://scontent.xx.fbcdn.net/v/t1.15752-0/p403x403/157181232_260601775599893_4404483419325055995_n.png?_nc_cat=102&ccb=1-3&_nc_sid=f79d6e&_nc_ohc=XrMKlKV7EwEAX-LV-Ms&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&tp=30&oh=72feb42faa0fdd193470e1be64cb72b5&oe=60A4DB67"
+          alt=""
+        />
+      </Link>
       <div className="s">
         {" "}
         <center>
